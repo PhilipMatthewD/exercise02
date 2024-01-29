8f89ae8c77348dc2bd4a2d2def1e6609a29f1f28
SloppyJalopy <matthewprof106@gmail.com>
2024-01-28 21:35:44 -0500

    exercise02

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] highTemp = {45,29,10,22,41,28,33};
+        String[] weekdays = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        int[] rainSpell = {95,60,25,5,0,75,90};
+        for (int i = 0; i < weekdays.length; i++) {
+            if (highTemp[i] <= 32 && rainSpell[i] > 50){
+                System.out.println("It is going to snow on "+weekdays[i]);
+            }
+        }
+    }
+}
